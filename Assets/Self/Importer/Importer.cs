4b15aeba2b7789065fa27ab4795c5b574cd8933c
iFroggo <c4xy@outlook.com>
2023-02-08 23:25:10 +0000

    object importer and auto creation

--- Assets/Self/Importer/Importer.cs
+++ Assets/Self/Importer/Importer.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System;
 using UnityEngine;
 using GLTFast;
 
-//basic import example
 public class Importer : MonoBehaviour
 {
+    [SerializeField]
+    private string filename; 
     async void Start()
     {
-        var filePath = "";
+        Import();
+    }
+    async void Import() {
+        var filePath = $"C:/Users/{Environment.UserName}/Downloads/{filename}.glb";
         byte[] data = File.ReadAllBytes(filePath);
         var gltf = new GltfImport();
         bool success = await gltf.LoadGltfBinary(data);
         if (success) {
-            success = await gltf.InstantiateMainSceneAsync(transform);
+            var placedModel = new GameObject(filename).transform;
+            success = await gltf.InstantiateMainSceneAsync( placedModel );
+            if(success) {
+                var invis = Instantiate(placedModel.gameObject,placedModel.gameObject.transform);
+                invis.gameObject.name = "Invis";
+                var comp = placedModel.gameObject.AddComponent<Object>();
+                comp.SetInvis(invis);
+            }
+            
+
         }
     }
-
 }
