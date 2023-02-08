cc683b13af6d26e8acb2a0bc2b6d8710f5ffba71
iFroggo <c4xy@outlook.com>
2023-02-08 23:15:53 +0000

    added automatic object setup

--- Assets/Self/Importer/Importer.cs
+++ Assets/Self/Importer/Importer.cs
@@ -4,18 +4,30 @@ using System.IO;
 using UnityEngine;
 using GLTFast;
 
-//basic import example
 public class Importer : MonoBehaviour
 {
+    [SerializeField]
+    private string filename; 
     async void Start()
     {
-        var filePath = "";
+        Import();
+    }
+    async void Import() {
+        var filePath = $"C:/Users/C4xy/Downloads/{filename}.glb";
         byte[] data = File.ReadAllBytes(filePath);
         var gltf = new GltfImport();
         bool success = await gltf.LoadGltfBinary(data);
         if (success) {
-            success = await gltf.InstantiateMainSceneAsync(transform);
+            var placedModel = new GameObject(filename).transform;
+            success = await gltf.InstantiateMainSceneAsync( placedModel );
+            if(success) {
+                var invis = Instantiate(placedModel.gameObject,placedModel.gameObject.transform);
+                invis.gameObject.name = "Invis";
+                var comp = placedModel.gameObject.AddComponent<Object>();
+                comp.SetInvis(invis);
+            }
+            
+
         }
     }
-
 }
--- Assets/Self/New System/Object.cs
+++ Assets/Self/New System/Object.cs
@@ -2,6 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+//TODO CREATING THE INVIS OBJECT
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MeshCollider))]
+[RequireComponent(typeof(XRGrabInteractable))]
+[RequireComponent(typeof(LineRenderer))]
 public class Object : MonoBehaviour
 {
     [SerializeField]
@@ -33,19 +41,80 @@ public class Object : MonoBehaviour
         isHeld = !isHeld;
         ChangeDrawings(isHeld);
     }
+    public bool SetInvis(GameObject invis) {
+        if(this.invis == null) {
+            this.invis = invis;
+            return true;
+        }
+        return false;
+    }
 
  
     void Start()
     {
         Setup();
+
+    }
+    void Reset() {
+        Setup();
     }
     protected void Setup() {
+
         line.enabled = false;
+
+        
+        var green = (Material)Resources.Load("GREEN", typeof(Material));
+        if(invis.TryGetComponent(out MeshRenderer mRend)) {
+            mRend.material = green;
+        }
+        var MeshRenderers = invis.GetComponentsInChildren<MeshRenderer>(true);
+        if(MeshRenderers.Length != 0) {
+            foreach(var mRendr in MeshRenderers) {
+                mRendr.material = green;
+            }
+        }
+
         invis.SetActive(false);
         line.startWidth = 0.1059608f;
         line.endWidth = 0.1059608f;
+        var rb = gameObject.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        var xrG = gameObject.GetComponent<XRGrabInteractable>();
+        xrG.throwOnDetach = false;
+        xrG.trackRotation = false;
+
+        
+
+
+        xrG.selectEntered.AddListener(OnSelectEntered);
+        xrG.selectExited.AddListener(OnSelectExited);
+        
+        var mCol = gameObject.GetComponent<MeshCollider>();
+        if(gameObject.TryGetComponent(out MeshFilter meshF)) {
+            mCol.sharedMesh = meshF.mesh;
+        } else {
+            var mshFilters = gameObject.GetComponentsInChildren<MeshFilter>(true);
+
+            //check mesh collider perf at some point too might make more sense to do box colliders
+            if(mshFilters.Length == 1) {
+                mCol.sharedMesh = mshFilters[0].mesh;
+            }  else {
+                // we probably want to make a box collider if theres one more than one since
+                // i dont't think a mesh collider with mutliple is accurate
+                mCol.sharedMesh = mshFilters[0].mesh;
+            }
+        }
+        gameObject.layer = 6;
+
     }
-    // Update is called once per frame
+
+    protected virtual void OnSelectEntered(SelectEnterEventArgs args) => SetHeld();
+
+    protected virtual void OnSelectExited(SelectExitEventArgs args) => SetHeld();
+
+
+
     void Update()
     {
         if(!isHeld) return;
