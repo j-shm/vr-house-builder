0eb13c09f7bc5ec44645159c5e9a68b9e02f5ce5
iFroggo <c4xy@outlook.com>
2023-02-09 18:34:23 +0000

    indivdual objects or all objects

--- Assets/Self/Importer/Importer.cs
+++ Assets/Self/Importer/Importer.cs
@@ -10,39 +10,41 @@ public class Importer : MonoBehaviour
     private string dirPath = $"C:/Users/{Environment.UserName}/Downloads/stuff/";
 
     [SerializeField]
-    private bool startImport = true;
+    private bool importEverything = true;
+
+    [SerializeField]
+    private string fileName;
 
     void Start()
     {
-        if(startImport) {
+        if(importEverything) {
             ImportAll();
-            startImport = false;
+        } else {
+            if(fileName != "" || fileName != null) {
+                Import(fileName);
+            }
         }
     }
-    void OnValidate()
-    {
-        if(startImport) {
-            ImportAll();
-            startImport = false;
-        }
-    }
-
 
     void ImportAll() {
         if(System.IO.Directory.Exists(dirPath)) {
             string[] files = System.IO.Directory.GetFiles(dirPath, "*.glb");
             foreach(var file in files) {
-                Import(file);
+                Import(file,false);
             }
         }
     }
 
-    async void Import(String file) {
-        byte[] data = File.ReadAllBytes(file);
+    async void Import(String file, bool name = true) {
+
+        byte[] data = name ? File.ReadAllBytes($"{dirPath}{file}.glb") : File.ReadAllBytes(file);
+
         var gltf = new GltfImport();
         bool success = await gltf.LoadGltfBinary(data);
         if (success) {
-            var placedModel = new GameObject(file.Split("/")[^1].Split(".")[0]).transform;
+
+            var placedModel = name ? new GameObject(file).transform : new GameObject(file.Split("/")[^1].Split(".")[0]).transform;
+
             success = await gltf.InstantiateMainSceneAsync( placedModel );
             if(success) {
                 var invis = Instantiate(placedModel.gameObject,placedModel.gameObject.transform);
@@ -52,5 +54,4 @@ public class Importer : MonoBehaviour
             }
         }
     }
-
 }
