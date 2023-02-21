7edbe524d07fde5f0515cc924dddefefc24c2ff5
iFroggo <c4xy@outlook.com>
2023-02-21 21:41:33 +0000

    further testing

---
+++ Assets/Self/Testing/GetPos.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPos : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log(this.gameObject.transform.position);
+    }
+}
