7438d049d5393e0478b377e411bc0ba849cab082
j-sh <j-shmf@proton.me>
2023-03-18 13:46:44 +0000

    cube maker

---
+++ Assets/Self/Testing/MakeObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeObject : MonoBehaviour
+{
+    public Collider col;
+    public bool make;
+
+    public void Update() {
+        if(col != null && make) {
+            CreateCube();
+            make = false;
+        }
+    }
+    public void CreateCube() {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.localScale = col.bounds.size;
+        cube.transform.position = this.gameObject.transform.position;
+    }
+}
